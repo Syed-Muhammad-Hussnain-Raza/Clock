cbc87b14e6d5595b6bd0747674093a9dfca48bbc
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-05 22:01:26 +0500

    Code - Updated using hooks

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,10 +1,20 @@
-let CurrentTime = () => {
-  let time = new Date();
+import { useState, useEffect } from "react";
 
+let CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   return (
     <p className="lead">
-      Current time: {time.toLocaleDateString()} -{" "}
-      {time.toLocaleTimeString()}
+      Current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}
     </p>
   );
 };
